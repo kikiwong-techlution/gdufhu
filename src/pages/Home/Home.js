584d4fdcfbebb3e7c4ad8a2c64cad57d23367667
movbenr <movbenr1016@yahoo.com>
2019-05-29 16:09:27 +0800

    add Quiz file

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -18,6 +18,56 @@ Page({
 
   },
 
+  /*搜索框切换面板*/
+  onBindFocus: function () {
+    this.setData({
+      pageShow:false,
+      searchPanelShow:true
+    })
+  },
+
+  /*隐藏搜索面板 */
+  onCancelImgTap: function () {
+    this.setData({
+      pageShow:true,
+      searchPanelShow:false,
+      inputValue:''
+    })
+  },
+
+  /*Navtab切换 */
+  switchNav: function (event) {
+    var that = this
+    if (this.data.currentTab === event.target.dataset.current) {
+      return false
+    }else {
+      that.setData({
+        currentTab:event.target.dataset.current,
+      })
+    }
+  },
+
+  swiperChange: function (event) {
+    this.setData({
+      currentTab:event.detail.current
+    })
+  },
+
+  /*提问页面跳转 */
+  onTabJump: function () {
+    wx.navigateTo({
+      url: '../Home/Quiz/Quiz',
+      success: function () {
+        console.log('Jump success')
+      },
+      fail: function () {
+        console.log('Jump fail')
+      },
+      complete: function () {
+        console.log('Jump complete')
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
